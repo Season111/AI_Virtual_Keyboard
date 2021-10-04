7ce28eeeaeadf7885231047579a9757d5bb603fe
Season <65497908+Season111@users.noreply.github.com>
2021-10-04 23:10:26 +0800

    Update AI Virtual Keyboard using OpenCV .py

--- AI Virtual Keyboard using OpenCV .py
+++ AI Virtual Keyboard using OpenCV .py
@@ -17,7 +17,7 @@ keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
 
-finalText = "SEASON111"
+finalText = ""
 
 keyboard = Controller()
 
